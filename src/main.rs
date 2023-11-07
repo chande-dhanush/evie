4a3706d1446b83507c8abe7536b8d677a94975f8
Aleph Null <purabisme@gmail.com>
2023-11-07 20:00:51 +0530

    random commit

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,12 @@ fn main() {
     let t = Tensor::from_slice(&[3, 1, 4, 1, 5]);
     let t = t * 2;
     t.print();
-    
+
     let mut beep_boop: Evie = Evie{
 
     };
+
+    name = "Head";
 
     beep_boop.init();
     beep_boop.run();
