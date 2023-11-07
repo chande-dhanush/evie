2c99d722fb874549092c7a79b9b6989c2bdd0478
Aleph Null <purabisme@gmail.com>
2023-11-07 19:13:16 +0530

    bug fix.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ pub mod evie;
 
 //Just an entry point.
 fn main() {
-    let beep_boop: Evie = Evie{
+    let mut beep_boop: Evie = Evie{
 
-    }
+    };
 
     beep_boop.init();
     beep_boop.run();
