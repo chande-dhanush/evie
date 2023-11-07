e84863d0ed19d2a26f9dc819237a1f86252a6e15
Aleph Null <purabisme@gmail.com>
2023-11-07 19:56:46 +0530

    testing commit

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,15 @@ use evie::Evie;
 
 pub mod evie;
 
+use tch::Tensor;
+
 //Just an entry point.
 fn main() {
+
+    let t = Tensor::from_slice(&[3, 1, 4, 1, 5]);
+    let t = t * 2;
+    t.print();
+    
     let mut beep_boop: Evie = Evie{
 
     };
